39083c1c30c4d97b8ae1586f8e99aa61be57e4e4
Jzheng777 <89608828+Jzheng777@users.noreply.github.com>
2025-05-13 14:37:46 -0400

    cors update 3

--- src/main/java/com/jzk/motif_backend/config/SecurityConfig.java
+++ src/main/java/com/jzk/motif_backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://www.motif-song.com", "http://localhost:3000","https://railway.app", "https://motif-song.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
